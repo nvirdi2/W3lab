94bffae710a92bede16de2427aad1b1c38e1c117
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-02-04 18:10:25 -0500

    Create Bill.h

---
+++ Bill.h
@@ -0,0 +1,35 @@
+// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: Feb 4, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
+#ifndef SDDS_BILL_H
+#define SDDS_BILL_H
+#include "Item.h"
+
+namespace sdds 
+{
+    class Bill 
+    {
+        char m_title[37];  // title of the shopping list
+        Item* m_items;     // pointer to the dynamically allocated array of Items
+        int m_noOfItems;   // size of the dynamically allocated array of Items
+        int m_itemsAdded;  // number of the Items added by the add method
+        double totalTax()const;   // returns the total tax applied to the Items
+        double totalPrice()const;  // returns the sum of prices of Items
+        void Title()const;       // Prints the title of the shopping list
+        // prints the footer contaning total tax, price and total price after tax
+        void footer()const;
+        void setEmpty();     // sets the Bill to an emtpy state
+        bool isValid()const;  // returns true is the Bill and all of its Items are valid
+
+    public:
+        void init(const char* title, int noOfItems);
+        bool add(const char* item_name, double price, bool taxed);
+        void display()const;
+        void deallocate();
+    };
+}  
+    #endif // !SDDS_ITEM_H
